88b8f35334288838ac1db474f020bbfa3f7bf439
agoldwater <dmgldwtr@gmail.com>
2017-08-21 21:01:41 -0500

    price difference change

--- store_items.rb
+++ store_items.rb
@@ -2,4 +2,4 @@ item_1 = {:name => "red ketchup", :color => "red", :cost => 2}
 item_2 = {name: "green ketchup", color: "green", cost: 3}
 item_3 = {:name => "mustard", :color => "yellow", :cost => 1}
 
-puts 
+puts "#{item_2[:name]} tastes the exact same as #{item_1[:name]} but costs more. $#{(item_2[:cost]-item_1[:cost])} dollar more!"
