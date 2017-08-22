d6b7254f42c23fb1aa79a0ec5fe88636f4cf7014
agoldwater <dmgldwtr@gmail.com>
2017-08-21 20:53:41 -0500

    initial commit

---
+++ store_items.rb
@@ -0,0 +1,5 @@
+item_1 = {:name => "red ketchup", :color => "red", :cost => 2}
+item_2 = {name: "green ketchup", color: "green", cost: 3}
+item_3 = {:name => "mustard", :color => "yellow", :cost => 1}
+
+puts 
