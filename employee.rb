7d8b3f0b20909b346b4397ee624f6e23d561a8b8
agoldwater <dmgldwtr@gmail.com>
2017-08-21 21:26:30 -0500

    adding class methods

--- employee.rb
+++ employee.rb
@@ -9,23 +9,40 @@
 # puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
 # puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
 
-# class Employee
+class Employee
 
-#   def initialize(input_first_name, input_last_name, input_salary, input_active)
-#     @first_name = input_first_name
-#     @last_name = input_last_name
-#     @salary = input_salary
-#     @active = input_active
-#   end
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
 
-#   def print_info
-#     puts "#{@first_name} #{@last_name} makes #{@salary} a year."
-#   end
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
 
-# end
+  def give_annual_raise
+    return @salary = @salary * 1.05
+  end
 
-# employee_1 = Employee.new("Jimmy", "Stewart", 70000, true)
-# employee_2 = Employee.new("Susan", "Messing", 80000, true)
+  def first_name
+    return @first_name
+  end
 
-# employee_1.print_info
-# employee_2.print_info
+  def last_name
+    @last_name
+  end
+
+end
+
+employee_1 = Employee.new("Jimmy", "Stewart", 70000, true)
+employee_2 = Employee.new("Susan", "Messing", 80000, true)
+
+employee_1.print_info
+employee_2.print_info
+
+employee_2.give_annual_raise
+employee_2.print_info
+
+p employee_2.first_name
--- store_items.rb
+++ store_items.rb
@@ -3,3 +3,4 @@ item_2 = {name: "green ketchup", color: "green", cost: 3}
 item_3 = {:name => "mustard", :color => "yellow", :cost => 1}
 
 puts "#{item_2[:name]} tastes the exact same as #{item_1[:name]} but costs more. $#{(item_2[:cost]-item_1[:cost])} dollar more!"
+puts "Please note, both #{item_1[:name]} and #{item_2[:name]} taste equallly as good as #{item_3[:name]} on a hot dog"
